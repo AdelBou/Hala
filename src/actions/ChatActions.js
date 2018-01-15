1eb87f7caff37ed0c303ea9b9c569b7af4614be2
bounouamustapha <em_bounoua@esi.dz>
2018-01-15 23:46:16 +0100

    hello

--- src/actions/ChatActions.js
+++ src/actions/ChatActions.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 import {Actions} from 'react-native-router-flux';
 import {
-    SET_USER, SEARCH_FRIEND, SET_MESSAGES,SET_FRIEND
+    SET_USER, SEARCH_FRIEND, SET_MESSAGES, SET_FRIEND
 } from './types';
 
 
@@ -41,80 +41,77 @@ export const set_messages = (user) => {
 
 
 export const search_friend = (user) => {
-    set_searchingForFriend(user,true);
-    let friend = {
-        id: "",
-        connected: false,
-        searchingForFriend: false,
-        name: "none",
-        chattingWith: null,
-    };
-
-    let timer = duration, minutes, seconds;
-    let timerId = setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-
-            console.log("minu:" + minutes + "seconds" + seconds);
-
-            if (--timer < 0) {
-            
-                clearInterval(timerId);
-                console.log("finished");
-            }
-
-        }
-            .bind(this), 1000
-    );
-
-
-
-
-    firebase.database().ref(`/users`).orderByChild("searchingForFriend").equalTo(true)
-        .once(
-            'value', snapshot => {
-                snapshot.forEach(function (childSnapshot) {
-                    if (childSnapshot.val().id !== user.id) {
-                        friend = childSnapshot.val();
-                    }
-                });
-
-                if (friend.name === 'none') {
-                    alert("did not find someone to fuck" + friend.name);
-
-                }
-                else {
-                         alert("found someone to fuck" + friend.name);
-                         set_friend(user,friend);
-                         set_searchingForFriend(user,false);
-                         set_searchingForFriend(friend,false);
-
-
-                }
-
-            }
-        );
-    // startTime(30, () => {
     return (dispatch) => {
         dispatch({
             type: SEARCH_FRIEND,
-            plyload: friend,
+            payload: true,
         });
+        set_searchingForFriend(user, true);
+        let friend = {
+            id: "",
+            connected: false,
+            searchingForFriend: false,
+            name: "none",
+            chattingWith: null,
+        };
+        firebase.database().ref(`/users/${user.id}`)
+            .on( 'value', snapshot => {
+                if (snapshot.val().chattingWith!==null){
+                    firebase.database().ref(`/users/${snapshot.val()}`).once(
+                        'value', snapshot => {
+                            friend=snapshot.val();
+                        }
+                    );
+                        set_friend(dispatch, user, friend);
+                        set_searchingForFriend(user, false);
+                        set_searchingForFriend(friend, false);
+                        dispatch({
+                            type: SET_FRIEND,
+                            payload: friend,
+                        });
+                }
+            });
+
+        firebase.database().ref(`/users`).orderByChild("searchingForFriend").equalTo(true)
+            .on(
+                'value', snapshot => {
+                    snapshot.forEach(function (childSnapshot) {
+                        if (childSnapshot.val().id !== user.id) {
+                            friend = childSnapshot.val();
+                        }
+                    });
+
+                    if (friend.name === 'none') {
+                        console.log("did not find someone to fuck" + friend.name);
+                    }
+                    else {
+                        console.log("found someone to fuck" + friend.name);
+                        set_friend(dispatch, user, friend);
+                        set_searchingForFriend(user, false);
+                        set_searchingForFriend(friend, false);
+                        dispatch({
+                            type: SET_FRIEND,
+                            payload: friend,
+                        });
+                    }
+
+                }
+            );
+
+
     };
 
 };
 
 
-set_searchingForFriend = (user,x) => {
-     firebase.database().ref(`/users/${user.id}`).child('searchingForFriend').set(x);
+set_searchingForFriend = (user, x) => {
+    firebase.database().ref(`/users/${user.id}`).child('searchingForFriend').set(x);
 };
 
 
-set_friend =(user,friend)=>{
-        firebase.database().ref(`/users/${user.id}`).child('chattingWith').set(friend.id);
-        firebase.database().ref(`/users/${friend.id}`).child('chattingWith').set(user.id);
+set_friend = (dispatch, user, friend) => {
+    firebase.database().ref(`/users/${user.id}`).child('chattingWith').set(friend.id);
+    firebase.database().ref(`/users/${friend.id}`).child('chattingWith').set(user.id);
 }
 
 
